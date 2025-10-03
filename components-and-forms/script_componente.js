48d83cf60cf0ea5871ebf6fea8c25bb219d2d3f9
Vinicius <viniciusgsimoni@gmail.com>
2025-10-03 20:26:27 -0300

    feat:display and styling improvements

--- components-and-forms/script_componente.js
+++ components-and-forms/script_componente.js
@@ -1,53 +1,54 @@
 lucide.createIcons();
 
 class AulasComponent extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-      this.hoje = "ter";
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.hoje = "ter";
+  }
   
-    connectedCallback() {
-      this.loadData();
-    }
+  connectedCallback() {
+    this.loadData();
+  }
   
-    async loadData() {
-      try {
-        const response = await fetch('aulas.json');
-        const aulas = await response.json();
-        this.render(aulas);
-      } catch (error) {
-        console.error('Erro ao carregar os dados das aulas:', error);
-      }
-    }
-  
-    render(aulas) {
-      const aulasDia = aulas.filter(a => a.data === this.hoje);
-  
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.href = 'styles_componente.css'; 
-      this.shadowRoot.appendChild(link); 
-  
-      this.shadowRoot.innerHTML += `
-        <div>
-          ${aulasDia.map(a => {
-            let provaDisplay = a.prova_alert ? '' : 'display: none;';
-            return `
-              <div class="comp-aula">
-                <div class="lable-prova p_lable" style="${provaDisplay}">PROVA: <b>${a.prova}</b></div>
-                <div class="titulo_aula">${a.disciplina}</div>
-                <p class="p">Local e Horário: <b>${a.local} - ${a.horario}</b></p>
-                <div class="lables">
-                  <div class="lable-frequencia p_lable">FALTAS: <b>${a.frequencia}</b></div>
-                  <div class="lable-nota p_lable">CR: <b>${a.nota}</b></div>
-                </div>
-              </div>
-            `;
-          }).join('')}
-        </div>
-      `;
+  async loadData() {
+    try {
+    const response = await fetch('aulas.json');
+    const aulas = await response.json();
+    this.render(aulas);
+    } catch (error) {
+    console.error('Erro ao carregar os dados das aulas:', error);
     }
   }
   
-  customElements.define('aulas-component', AulasComponent);  
+  render(aulas) {
+    const aulasDia = aulas.filter(a => a.data === this.hoje);
+  
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'styles_componente.css'; 
+    this.shadowRoot.appendChild(link); 
+  
+    this.shadowRoot.innerHTML += `
+    <div>
+      ${aulasDia.map(a => {
+      let provaDisplay = a.prova_alert ? '' : 'display: none;';
+      let notaColor = a.nota < 6 ? '#f32222ff' : a.nota < 8 ? '#ffd000ff' : '#00ff00';
+      return `
+        <div class="comp-aula">
+        <div class="lable-prova p_lable" style="${provaDisplay}">PROVA: <b>${a.prova}</b></div>
+        <div class="titulo_aula">${a.disciplina}</div>
+        <p class="p">Local e Horário: <b>${a.local} - ${a.horario}</b></p>
+        <div class="lables">
+          <div class="lable-frequencia p_lable">FALTAS: <b>${a.frequencia}</b></div>
+          <div class="lable-nota p_lable">CR: <b style="color: ${notaColor};">${a.nota}</b></div>
+        </div>
+        </div>
+      `;
+      }).join('')}
+    </div>
+    `;
+  }
+  }
+  
+  customElements.define('aulas-component', AulasComponent);
